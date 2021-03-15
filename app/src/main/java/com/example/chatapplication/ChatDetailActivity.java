093527096146eca8f9705b8a12fec5f25121ecb5
afkrohan <r_o_h_a_n_99@outlook.com>
2021-03-15 12:28:48 +0530

    4:00hrs

--- app/src/main/java/com/example/chatapplication/ChatDetailActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatDetailActivity.java
@@ -1,15 +1,29 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
 
+import com.example.chatapplication.Adapters.ChatAdapter;
+import com.example.chatapplication.Models.MessagesModel;
 import com.example.chatapplication.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatDetailActivity extends AppCompatActivity {
 
@@ -26,7 +40,7 @@ public class ChatDetailActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
 
-        String senderId = auth.getUid();
+        final  String senderId = auth.getUid();
         String recievedId = getIntent().getStringExtra("userId");
         String userName = getIntent().getStringExtra("userName");
         String profilePic = getIntent().getStringExtra("profilePic");
@@ -42,6 +56,35 @@ public class ChatDetailActivity extends AppCompatActivity {
             }
         });
 
+        final ArrayList <MessagesModel> messagesModels = new ArrayList<>();
 
+        final ChatAdapter chatAdapter = new ChatAdapter(messagesModels, this );
+
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId + recievedId;
+        final  String receiverRoom = recievedId + senderId;
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message =  binding.etmessage.getText().toString();
+                final MessagesModel model = new MessagesModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.etmessage.setText("");
+                database.getReference().child("chats").child("senderRoom").push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        database.getReference().child("chats").child("receiverRoom").push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 }
