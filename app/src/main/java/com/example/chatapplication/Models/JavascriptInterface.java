03871acd0b9ef871211253431bdc53a3093e322a
Vraj Rana <vrcoder1998@gmail.com>
2021-04-22 10:38:08 +0530

    Video call update

---
+++ app/src/main/java/com/example/chatapplication/Models/JavascriptInterface.java
@@ -0,0 +1,14 @@
+package com.example.chatapplication.Models;
+
+import com.example.chatapplication.VideoCallActivity;
+//import android.webkit.JavascriptInterface;
+
+public class JavascriptInterface {
+
+    VideoCallActivity videoCallActivity;
+
+    public void onPeerConnected(){
+        //videoCallActivity.onPeerConnected();
+    }
+
+}
--- app/src/main/java/com/example/chatapplication/VideoCallActivity.java
+++ app/src/main/java/com/example/chatapplication/VideoCallActivity.java
@@ -1,10 +1,21 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
@@ -14,43 +25,41 @@ import java.net.URL;
 
 public class VideoCallActivity extends AppCompatActivity {
 
-    String username = "";
-    String friendsUsername = "";
-
-    String isPeerConnected = "";
-
-    Boolean isVideo = true;
-    Boolean isAudio = true;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videocall);
 
-//        Intent intent = getIntent();
-//        String senderRoom = intent.getStringExtra("sRoom");
-//        String receiverRoom = intent.getStringExtra("rRoom");
-//
-//        try {
-//            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-//                    .setServerURL(new URL(""))
-//                    .setWelcomePageEnabled(false)
-//                    .build();
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//        setup_video_call(senderRoom);
+        Intent intent = getIntent();
+        String caller = intent.getStringExtra("userName");
+        String receiver = intent.getStringExtra("friend");
+
+        String senderRoom = intent.getStringExtra("senderRoom");
+
+
+        try {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL(""))
+                    .setWelcomePageEnabled(false)
+                    .build();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        setup_video_call(senderRoom);
     }
 
-//    public void setup_video_call(String senderRoom){
-//
-//        if( senderRoom.length() > 0 ) {
-//            JitsiMeetConferenceOptions options =
-//                    new JitsiMeetConferenceOptions.Builder()
-//                            .setRoom(senderRoom)
-//                            .build();
-//            JitsiMeetActivity.launch(this, options);
-//        }
-//    }
+
+
+    public void setup_video_call(String senderRoom){
+
+        if( senderRoom.length() > 0 ) {
+            JitsiMeetConferenceOptions options =
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setRoom(senderRoom)
+                            .build();
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+
 
 }
