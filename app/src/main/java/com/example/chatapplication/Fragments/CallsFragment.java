22973ced61328b1ca0898daaf536c2ceb646ff98
afkrohan <r_o_h_a_n_99@outlook.com>
2021-04-25 18:38:36 +0530

    notifi teesting

--- app/src/main/java/com/example/chatapplication/Fragments/CallsFragment.java
+++ app/src/main/java/com/example/chatapplication/Fragments/CallsFragment.java
@@ -1,12 +1,11 @@
 package com.example.chatapplication.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.chatapplication.R;
 
@@ -20,6 +19,7 @@ public class CallsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
+
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
